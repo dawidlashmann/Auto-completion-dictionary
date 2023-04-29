479c313ed1c70510fdcb7fc6283e6f3b43bba0a1
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-04-29 20:53:00 +0200

    add insert method definition

--- dictionary.cpp
+++ dictionary.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
-#include <memory>
+#include <unordered_map>
+#include <vector>
 
 #define alphabet 26
 
 struct TrieNode
 {
     bool is_end_of_word;
-    TrieNode *next_char[alphabet];
+    std::unordered_map<char, TrieNode *> children;
 };
 
 class Trie
 {
 public:
-    Trie() : root(std::make_unique<TrieNode>()){};
-    
-    void insert(std::string &);
-    void remove(std::string &);
-    // std::vector<std::string> candidates();
+    Trie();
+
+    void insert(const std::string &);
+    void remove(const std::string &);
+    std::vector<std::string> candidates;
 
     ~Trie(){};
 
 private:
-    std::unique_ptr<TrieNode> root;
+    TrieNode *root;
 };
 
-void Trie::insert(std::string &word)
+void Trie::insert(const std::string &query)
 {
+    TrieNode *current_node = root;
+    for (char letter : query)
+    {
+        if (current_node->children.find(letter) == current_node->children.end())
+        {
+            current_node->children[letter] = new TrieNode;
+        }
+        current_node = current_node->children[letter];
+    }
+    current_node->is_end_of_word = true;
 }
 
-void Trie::remove(std::string &word)
+void Trie::remove(const std::string &query)
 {
     
 }
