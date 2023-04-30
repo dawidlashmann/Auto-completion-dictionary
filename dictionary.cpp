0e117706f2639e319f79c2b4c3b3dc51defa9129
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-04-30 02:02:11 +0200

    add traverse method

--- dictionary.cpp
+++ dictionary.cpp
@@ -6,7 +6,7 @@
 
 struct TrieNode
 {
-    bool is_end_of_word;
+    bool is_end_of_word = false;
     std::unordered_map<char, TrieNode *> children;
 };
 
@@ -17,18 +17,21 @@ public:
 
     void insert(const std::string &);
     void remove(const std::string &);
-    std::vector<std::string> candidates;
+    std::vector<std::string> candidates(const std::string &);
 
     ~Trie(){};
+
+private:
+    char traverse(TrieNode *, const std::string &, char, std::vector<std::string>&);
 
 private:
     TrieNode *root;
 };
 
-void Trie::insert(const std::string &query)
+void Trie::insert(const std::string &word)
 {
     TrieNode *current_node = root;
-    for (char letter : query)
+    for (char letter : word)
     {
         if (current_node->children.find(letter) == current_node->children.end())
         {
@@ -39,9 +42,46 @@ void Trie::insert(const std::string &query)
     current_node->is_end_of_word = true;
 }
 
-void Trie::remove(const std::string &query)
+void Trie::remove(const std::string &word)
 {
-    
+    ///
+}
+
+std::vector<std::string> Trie::candidates(const std::string &query)
+{
+    std::vector<std::string> auto_completed_queries;
+    TrieNode *current_node = root;
+    for (char letter : query)
+    {
+        if (current_node->children.find(letter) == current_node->children.end())
+        {
+            std::cout << "No candidates for this query\n";
+            return auto_completed_queries;
+        }
+        current_node = current_node->children[letter];
+    }
+
+    return auto_completed_queries;
+}
+
+char Trie::traverse(TrieNode *current_node, const std::string &prefix, char node_char, std::vector<std::string>& result)
+{
+    static std::string current_txt = "";
+    current_txt = current_txt + node_char;
+    if (current_node->children.empty())
+    {
+        return node_char;
+    }
+
+    if (current_node->is_end_of_word)
+    {
+        result.emplace_back(prefix + current_txt);
+    }
+
+    for (auto &node : current_node->children)
+    {
+        result.emplace_back(prefix + traverse(node.second, prefix, node.first));
+    }
 }
 
 int main()
